b6c0e2674141486a12979a0870ef8f24bf08970d
Anurag Mishra <anurag.mishra150304@gmail.com>
2025-02-17 01:00:51 +0530

    file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,13 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    server: {
-        port: 3000,
-        proxy: {
-            '/api': {
-                target: 'https://recallify-backend.onrender.com',
-                changeOrigin: true,
-            },
-        },
-    },
 }) 
